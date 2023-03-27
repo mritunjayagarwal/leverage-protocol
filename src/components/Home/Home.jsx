2b90b001987866dc457e0a16b874194a037f5d1d
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-03-28 01:31:53 +0530

    Link Added on home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <div className='text-center'>
+        <h1>Hi! Welcome to Leverage Protocol</h1>
+        <Link to = "/swap" className='btn btn-primary py-2 px-5'>Go to Swap</Link>
+        </div>
+    </>
   )
 }
 
